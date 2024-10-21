1d96256bbc1faa8b5175e53b8cf57457f0f37b54
shubechavan <142329297+shubechavan@users.noreply.github.com>
2024-10-21 10:17:31 +0530

    Update admin.js

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,4 +1,4 @@
-console.log('Admin middleware loaded'); // Add this at the top
+console.log('Admin middleware loaded'); 
 
 const jwt = require('jsonwebtoken');   
 
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 const secretKey = process.env.JWT_SECRET || 'your_secret_key';
 
 function isAdmin(req, res, next) {
-    const token = req.headers['authorization']?.split(' ')[1]; // Get token from Authorization header
+    const token = req.headers['authorization']?.split(' ')[1]; 
 
     if (!token) {
         return res.status(403).json({ message: 'Access denied. No token provided.' });
@@ -19,13 +19,13 @@ function isAdmin(req, res, next) {
             return res.status(403).json({ message: 'Invalid token.' });
         }
 
-        // Assuming your admin data is stored in the JWT
+     
         if (decoded.role !== 'admin') {
             return res.status(403).json({ message: 'Access denied. Not an admin.' });
         }
 
-        req.user = decoded; // Attach user info to the request object
-        next(); // Proceed to the next middleware or route handler
+        req.user = decoded; 
+        next();
     });
 }
 
